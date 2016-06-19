ef1fc6abd3e2bbeae76be49c3fd9ae70492ecfed
Giustino Borzacchiello <giustinob@gmail.com>
2016-06-19 16:21:02 +0200

    Decrement counter by click

--- js/app.js
+++ js/app.js
@@ -11,12 +11,17 @@ var Counter = React.createClass( {
 			counter: this.state.counter + 1
 		});
 	},
+	decrement: function() {
+		this.setState({
+			counter: this.state.counter - 1
+		});
+	},
 	render: function () {
 		return (
 			<div>
 				<h2>{this.state.counter}</h2>
 				<button onClick={this.increment}>Increment</button>
-				<button>Decrement</button>
+				<button onClick={this.decrement}>Decrement</button>
 			</div>
 		);
 	}
