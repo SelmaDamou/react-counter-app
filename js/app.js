a84d3776fe5f29540a070abc4830f6b0a7526bfa
Giustino Borzacchiello <giustinob@gmail.com>
2016-06-19 17:41:15 +0200

    Replace tabs with spaces

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 "use strict";
 
 var Counter = React.createClass( {
-	getInitialState: function () {
-		return {
-			counter: 0
-		}
-	},
-	increment: function () {
-		this.setState( {
-			counter: this.state.counter + 1
-		} );
-	},
-	decrement: function () {
-		this.setState( {
-			counter: this.state.counter - 1
-		} );
-	},
-	render: function () {
-		return (
-			<div>
-				<h2>{this.state.counter}</h2>
-				<button className="pure-button button-error" onClick={this.decrement}>Decrement</button>
-				<button className="pure-button button-success" onClick={this.increment}>Increment</button>
-			</div>
-		);
-	}
+  getInitialState: function () {
+    return {
+      counter: 0
+    }
+  },
+  increment: function () {
+    this.setState( {
+      counter: this.state.counter + 1
+    } );
+  },
+  decrement: function () {
+    this.setState( {
+      counter: this.state.counter - 1
+    } );
+  },
+  render: function () {
+    return (
+      <div>
+        <h2>{this.state.counter}</h2>
+        <button className="pure-button button-error" onClick={this.decrement}>Decrement</button>
+        <button className="pure-button button-success" onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
 } );
 
 ReactDOM.render( <Counter />, document.getElementById( 'mountpoint' ) );
