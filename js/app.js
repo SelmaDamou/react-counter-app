56b5e50ba820119f8257b2a5daa6d2fb2a4f7d4b
Giustino Borzacchiello <giustinob@gmail.com>
2016-06-19 16:01:51 +0200

    Add React and simple component

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 "use strict";
 
 console.log( "A simple counter" );
+
+var Counter = React.createClass( {
+	render: function () {
+		return <p>Hello I'm a counter created with React</p>
+	}
+} );
+
+ReactDOM.render( <Counter />, document.getElementById( 'mountpoint' ) );
